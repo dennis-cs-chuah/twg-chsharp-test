cae727de7ab9484f2d3e630850e5eac8852e2d5e
Dennis Chuah <dennis@inetgardens.com>
2022-08-02 14:23:01 +1200

    Added search

--- ProductController.cs
+++ ProductController.cs
@@ -4,9 +4,17 @@ namespace CSharpTest
 {
     public class ProductController : Controller
     {
-        public IActionResult Index ()
+        [Route("/Seach"), HttpGet]
+        public IActionResult Search ([FromQuery] string q)
         {
-            return View ();
+            // Search
+            HttpClient searchClient = new ();
+            string url = "https://twg.azure-api.net/bolt/search.json?UserId=21E3BC8B-CA74-4C9A-9A0F-F0748A550B92&Search=" + q;
+            HttpResponseMessage searchResponse = searchClient.GetAsync (url).Result;
+            string searchResult = searchResponse.Content.ReadAsStringAsync ().Result;
+
+            
+            return new EmptyResult ();
         }
     }
 }
