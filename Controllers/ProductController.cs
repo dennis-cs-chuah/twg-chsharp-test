54b924f96d3d401f31c19648ffccc518a394f24d
Shane Harris <shane@threebrownbears.co.nz>
2022-08-03 15:33:17 +1200

    Added basic logging
    Basic logging added, appsettings used

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CSharpTest.Controllers {
+    [ApiController]
+    [Route ("[controller]")]
+    public class ProductController : ControllerBase {
+        private IConfiguration configuration;
+
+        public ProductController(IConfiguration iconfig) {
+            configuration = iconfig;
+
+        }
+
+        [Route ("Search"), HttpGet]
+        public IActionResult SearchAsync([FromQuery] string q) {
+            string SubscriptionKey = configuration.GetValue<string> ("SUB_KEY");
+            string ConnectionString = configuration.GetValue<string> ("ConnectionStrings:DB");
+            // Search
+            HttpClient searchClient = new ();
+            string url = "https://twg.azure-api.net/bolt/search.json?UserId=21E3BC8B-CA74-4C9A-9A0F-F0748A550B92&Search=" + q;
+            HttpResponseMessage searchResponse = searchClient.GetAsync (url).Result;
+            string searchResult = searchResponse.Content.ReadAsStringAsync ().Result;
+
+            // Log
+            SqlConnection connection = new SqlConnection (ConnectionString);
+            connection.Open ();
+            string sql = "INSERT INTO devtest.SearchRequest (Rid, Search, SuccessInd, Hits) VALUES ('" + new Random ().Next () + "',' " + q + "','Y','1')";
+            SqlCommand command = new SqlCommand (sql, connection);
+            command.ExecuteNonQuery ();
+
+            return new OkObjectResult (searchResult);
+        }
+
+        [Route ("Price"), HttpGet]
+        public IActionResult Price([FromQuery] string q) {
+            string SubscriptionKey = configuration.GetValue<string> ("SUB_KEY");
+            string ConnectionString = configuration.GetValue<string> ("ConnectionStrings:DB");
+            HttpClient priceClient = new ();
+            string url = "https://twg.azure-api.net/bolt/price.json?UserId=21E3BC8B-CA74-4C9A-9A0F-F0748A550B92&MachineID=test&Barcode=" + q;
+            HttpResponseMessage searchResponse = priceClient.GetAsync (url).Result;
+            string searchResult = searchResponse.Content.ReadAsStringAsync ().Result;
+            return new OkObjectResult (searchResult);
+
+            // log 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,20 @@
-var builder = WebApplication.CreateBuilder (args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages ();
 
-var app = builder.Build ();
+builder.Services.AddControllers();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment ())
-{
+if (!app.Environment.IsDevelopment ()) {
     app.UseExceptionHandler ("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts ();
 }
 
-app.UseHttpsRedirection ();
-app.UseStaticFiles ();
+app.UseAuthorization();
 
-app.UseRouting ();
+app.MapControllers();
 
-app.UseAuthorization ();
-
-app.MapRazorPages ();
-
-app.Run ();
+app.Run();
